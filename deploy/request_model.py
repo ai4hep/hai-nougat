4b6b9439401227adc794c5552369f91ff2efba5a
luojianwen21@mails.ucas.ac.cn <luojianwen21@mails.ucas.ac.cn>
2023-12-09 02:03:12 -0500

    nougat worker 部署

--- deploy/request_model.py
+++ deploy/request_model.py
@@ -1,19 +1,12 @@
+import os
 import hai
-from pathlib import Path
-import hai_model
+hai.api_key = os.getenv("HEPAI_API_KEY")
 
+ret = hai.Model.inference(
+    model='meta/nougat-test',
+    input='hello',
+    file='xxx/ddd.pdf',
+    xxx='xx',
+    )
 
-# 发起API请求，传递文件的字节数据
-ret = hai_model.HaiModel.inference(
-    model= r'meta/nougat',
-    timeout=90,
-    stream=False,
-    url= "http://aiapi.ihep.ac.cn:42901",
-    pdf_path = "/home/luojw/VSProjects/hai-nougat/HaiNougat/nougat.pdf",
-    api_key="Hi-VTAYsGaZmTIouTCOfSBeThEHyEuLfGhrpRDFODkfFkZkpEu",
-    start=10,
-    stop=10
-)
 print(ret)
-save_path = Path('/home/luojw/VSProjects/hai-nougat/test.md')
-save_path.write_text(ret, encoding="utf-8")
--- deploy/run_worker.sh
+++ deploy/run_worker.sh
@@ -1,6 +1,5 @@
-nohup python nougat_worker.py \
- --name meta/nougat \
- --host 0.0.0.0 \
+python nougat_worker.py \
+ --name meta/nougat-test \
  --controller_address http://aiapi.ihep.ac.cn:42901 \
  --limit_model_concurrency 10 \
  --permissions "groups: all; owner: hepai@ihep.ac.cn"  # 自行修改
